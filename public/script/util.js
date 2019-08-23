79196c93164633bebc311fb4752515e7ae519f27
Filipe <filipe.ramon7@gmail.com>
2019-08-23 18:12:46 -0300

    teste fedout

---
+++ public/script/util.js
@@ -0,0 +1,10 @@
+
+function fadeout() {
+    console.log(document.getElementById('container'));
+    window.location.href='/about/skills';
+    document.getElementById('container').style.opacity = '0';
+}
+
+function fadein() { 
+
+}
